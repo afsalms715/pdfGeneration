7f712d38b7e7a08e13ad1278f6b9609b1a70af75
afsalms715 <afsalms715@gamil.com>
2023-08-16 22:54:51 +0300

    landscap pdf

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+
+
 function generatePdf() {
     const {jsPDF}=window.jspdf;
-    var content = document.getElementById('pdf-content');
+    var content = document.getElementById('pdf-image');
 
     // Create a new jsPDF instance
-    var pdf = new jsPDF();
+    var pdf = new jsPDF('landscape');
 
     const pageHeight = pdf.internal.pageSize.height;
         const contentHeight = content.clientHeight;
@@ -24,8 +26,59 @@ function generatePdf() {
     });
 }
 
-    // Get the content from the HTML element
-    //margin: [10, 10, 10, 10],
+function toimage(){
+    
+    var node = document.getElementById('contents');
+    var pdgimage = document.getElementById('pdf-image');
 
-    // Save the PDF
-    //pdf.save('generated-pdf.pdf');
+/*htmlToImage.toPng(node)
+  .then(function (dataUrl) {
+    var img = new Image();
+    img.src = dataUrl;
+    pdgimage.innerHTML=`<img src='${dataUrl}' height='290px' width:'210px'/>`
+  })
+  .catch(function (error) {
+    console.error('oops, something went wrong!', error);
+  });*/
+  htmlToImage.toCanvas(node)
+  .then(function(canvas){
+    pdgimage.appendChild(canvas)
+  })
+}
+
+fetch('https://dummyjson.com/products/1')
+.then(res => res.json())
+.then(console.log);
+var products=[]
+function addProduct(){   
+    var productId=document.getElementById("productId")
+    if(productId.value){
+        productId.border='' 
+        var apiurl=`https://dummyjson.com/products/${productId.value}`
+        fetch(apiurl)
+        .then(res => res.json())
+        .then((resp)=>{
+            products.push({'id':resp.id,'desc':resp.title,'price':resp.price})
+            tableload()
+        });
+    }else{
+        productId.border='1px solid red' 
+    }
+}
+
+function tableload(){
+    if(products.length<=6){
+        var tablebody=document.getElementById("table-body")
+        var tabledata=''
+        products.map((resp)=>{
+            tabledata+=`<tr>
+                            <td>${resp.id}</td>
+                            <td>${resp.desc}</td>
+                            <td>${resp.price}</td>
+                        </tr>`
+        })
+        tablebody.innerHTML=tabledata
+    }else{
+        alert("only 6 items at a print")
+    }
+}
